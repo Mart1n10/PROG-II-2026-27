de740100f44278de0527e45dcefa644d4a721da7
MartínGómezPorras <martingomezporras7@gmail.com>
2026-09-16 17:30:15 +0200

    Revert "Delete PR2.POO-Martín-JugadorBaloncesto directory"
    This reverts commit 71c5f1c175aa3561d83032dfa10ce7d28d005302.

---
+++ PR2.POO-Martín-JugadorBaloncesto/Martín_Gómez_PR2.POO-JugadorBaloncesto/JugadorBaloncesto.cpp
@@ -0,0 +1,13 @@
+#include "JugadorBaloncesto.h"
+#include <iostream>
+using namespace std;
+
+void JugadorBaloncesto::Lanzar()
+{
+	cout << "El jugador a lanzado" << endl;
+}
+
+void JugadorBaloncesto::Saltar()
+{
+	cout << "El jugador a saltado" << endl;
+}
---
+++ PR2.POO-Martín-JugadorBaloncesto/Martín_Gómez_PR2.POO-JugadorBaloncesto/Martín_Gómez_PR2.POO-JugadorBaloncesto.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "JugadorBaloncesto.h"
+
+int main()
+{
+	JugadorBaloncesto jb;
+	jb.Lanzar();
+	jb.Saltar();
+}
