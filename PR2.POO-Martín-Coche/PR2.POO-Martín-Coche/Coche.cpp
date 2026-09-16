391b3f87bbfdcab9d6acaa1b5a26715d358096c8
MartínGómezPorras <martingomezporras7@gmail.com>
2026-09-16 17:30:10 +0200

    Revert "Delete PR2.POO-Martín-Coche directory"
    This reverts commit 74558711b29e9eb5287e73ef5efa64465b144b14.

---
+++ PR2.POO-Martín-Coche/PR2.POO-Martín-Coche/Coche.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Coche.h"
+
+void Coche::Acelerar()
+{
+	cout << "Estas acelerando" << endl;
+}
+
+void Coche::Frenar()
+{
+	cout << "Estas frenando" << endl;
+}
+
+void Coche::Girar()
+{
+	cout << "Estas girando" << endl;
+}
