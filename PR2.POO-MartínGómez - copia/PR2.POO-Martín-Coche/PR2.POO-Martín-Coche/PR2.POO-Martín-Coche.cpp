31c206de6acf1069512b8818806974a52e738003
MartínGómezPorras <martingomezporras7@gmail.com>
2026-09-16 18:09:38 +0200

    l

---
+++ PR2.POO-MartínGómez - copia/PR2.POO-Martín-Coche/PR2.POO-Martín-Coche/PR2.POO-Martín-Coche.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include "Coche.h"
+using namespace std;
+
+int main() {
+    Coche coche;
+
+    coche.Acelerar();
+    coche.Frenar();
+    coche.Girar();
+
+    return 0;
+}
