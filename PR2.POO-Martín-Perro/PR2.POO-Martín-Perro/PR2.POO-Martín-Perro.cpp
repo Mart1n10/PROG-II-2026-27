ec852caf6d7af658c1334cee8365766222ef536d
MartínGómezPorras <martingomezporras7@gmail.com>
2026-09-16 17:29:56 +0200

    Revert "Delete PR2.POO-Martín-Perro directory"
    This reverts commit 87045afa0377767c8db72d1697885c642173873d.

---
+++ PR2.POO-Martín-Perro/PR2.POO-Martín-Perro/PR2.POO-Martín-Perro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include "Perro.h"
+
+using namespace std;
+
+int main()
+{
+    Perro perro;
+    
+    perro.Correr();
+    perro.Dormir();
+
+    return 0;
+}
