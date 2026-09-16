c22754975d13db6eb6caa9ae89192f63d4df466e
MartínGómezPorras <martingomezporras7@gmail.com>
2026-09-16 17:13:55 +0200

    PR2.POO-MartínGómez

---
+++ PR2.POO-Martín-Perro/PR2.POO-Martín-Perro/Perro.cpp
@@ -0,0 +1,13 @@
+#include "Perro.h"
+#include <iostream>
+using namespace std;
+
+void Perro::Correr()
+{
+	cout << "El perro esta corriendo" << endl;
+}
+
+void Perro::Dormir()
+{
+	cout << "El perro esta durmiendo" << endl;
+}
